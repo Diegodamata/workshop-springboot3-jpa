c319a69cccca12223a4827b3ff36c7e4e5f084a2
Diego da Mata <Diegodamata>
2024-09-29 09:32:52 -0300

    User update

--- src/main/java/com/diegodev/course/services/UserServices.java
+++ src/main/java/com/diegodev/course/services/UserServices.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.diegodev.course.entities.User;
 import com.diegodev.course.repositories.UserRepository;
 
+import jakarta.transaction.Transactional;
+
 //@Component //registrando como componente, e ele agora vai poder ser injetado automaticamento em outras classes atraves do Autowired
 @Service //faz a mesma coisa que o component porem eu informo que é um serviço, como ja estou na minha classe de serviço vou usala
 public class UserServices {
@@ -43,5 +45,23 @@ public class UserServices {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	@Transactional //precisei colocar essa anotação pois estava dando um lazy loading, então informei que essa minha operação estara
+	//dentro de uma transação dessa forma é garantido que todas as alterações sejam feitas em uma transação e tendo sucesso 
+	//metodo para atualizar um usuario, preciso do id do usuario e o User responsavel por esse id
+	public User update(Long id, User obj) {
+		//o findById ele me tras do banco o usuario com base no id, ja o getReferenceById ele prepara o objeto para uma alteração
+		User entity = repository.getReferenceById(id);
+		
+		updateData(entity, obj);
+		
+		return repository.save(entity);
+	}
+	
+	public void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
